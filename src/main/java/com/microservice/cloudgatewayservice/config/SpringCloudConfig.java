694e913d23cda8b4af64a1164f7881aec09b7f95
JAGJEEVAN GOPIREDDY <jgopireddy@JAGJEEVANs-MacBook-Pro.local>
2020-07-19 11:04:19 -0400

    updates

--- src/main/java/com/microservice/cloudgatewayservice/config/SpringCloudConfig.java
+++ src/main/java/com/microservice/cloudgatewayservice/config/SpringCloudConfig.java
@@ -5,15 +5,24 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import lombok.Data;
+import reactor.core.publisher.Mono;
+
 @Configuration
 public class SpringCloudConfig {
 
-	/*@Bean
+	@Bean
 	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
 		return builder.routes()
 				.route(r -> r.path("/customer/**")
 						.filters(
-								f -> f.addRequestHeader("customer-req-header", "customer-request")
+								f -> f
+									.modifyRequestBody(IncomingRequest.class, CustomerRequest.class, 
+										(exchange, s) ->{
+											return Mono.just(new CustomerRequest(s).invoke());
+										}
+										)
+									.addRequestHeader("customer-req-header", "customer-request")
 									.addResponseHeader("customer-resp-header", "customer-response")
 								)
 						.uri("http://localhost:8081/")
@@ -28,5 +37,32 @@ public class SpringCloudConfig {
 						.id("transaction-service"))
 				
 				.build();
-	}*/
+	}
+	
+	@Data
+	public static class IncomingRequest{
+		private String id;
+		private String name;
+		private double salary;
+	}
+	
+	@Data
+	public static class CustomerRequest{
+		private String number;
+		private String firstName;
+		private double salary;
+		
+		private IncomingRequest incomingRequest;
+		
+		public CustomerRequest(IncomingRequest incomingRequest) {
+			this.incomingRequest = incomingRequest;
+		}
+		
+		public CustomerRequest invoke() {
+			this.setFirstName(incomingRequest.getName());
+			this.setNumber(incomingRequest.getId());
+			this.setSalary(incomingRequest.getSalary());
+			return this;
+		}
+	}
 }
